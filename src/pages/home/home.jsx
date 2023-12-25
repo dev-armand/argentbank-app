9c53ceb178469c69677c80b2006b93cfbbebe6a7
Armand Bredean <armand.bredean@gmail.com>
2023-12-25 19:34:21 +0100

    update

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,7 @@
 import '../../index.css';
 import Header from "../../components/header/header";
 import Footer from "../../components/footer/footer";
+import FeatureItem from '../../components/feature-item/feature-item';
 
 import IconChat from "../../assets/img/icon-chat.png"
 import IconSecurity from "../../assets/img/icon-security.png"
@@ -22,29 +23,21 @@ function Home() {
         </div>
         <section className="features">
           <h2 className="sr-only">Features</h2>
-          <div className="feature-item">
-            <img src={IconChat} alt="Chat Icon" className="feature-icon" />
-            <h3 className="feature-item-title">You are our #1 priority</h3>
-            <p>
-              Need to talk to a representative? You can get in touch through our
-              24/7 chat or through a phone call in less than 5 minutes.
-            </p>
-          </div>
-          <div className="feature-item">
-            <img src={IconMoney} alt="Chat Icon" className="feature-icon" />
-            <h3 className="feature-item-title">More savings means higher rates</h3>
-            <p>
-              The more you save with us, the higher your interest rate will be!
-            </p>
-          </div>
-          <div className="feature-item">
-            <img src={IconSecurity} alt="Chat Icon" className="feature-icon" />
-            <h3 className="feature-item-title">Security you can trust</h3>
-            <p>
-              We use top of the line encryption to make sure your data and money
-              is always safe.
-            </p>
-          </div>
+          <FeatureItem
+          icon={IconChat}
+          title="You are our #1 priority"
+          description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
+          />
+          <FeatureItem
+            icon={IconMoney}
+            title="More savings means higher rates"
+            description="The more you save with us, the higher your interest rate will be!"
+          />
+          <FeatureItem
+            icon={IconSecurity}
+            title="Security you can trust"
+            description="We use top of the line encryption to make sure your data and money is always safe."
+          />
         </section>
       </main>
       <Footer />
